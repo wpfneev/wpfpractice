6672de7bfd39ee054e78b2cf4123ec8b480e772b
Abhay Kumar Sharma <abhay.s@neevsystems.com>
2023-02-07 17:02:25 +0530

    Added abhay

---
+++ WindowHiding/WindowHiding/ChildWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WindowHiding
+{
+    /// <summary>
+    /// Interaction logic for ChildWindow.xaml
+    /// </summary>
+    public partial class ChildWindow : Window
+    {
+        private bool _Close;
+        public ChildWindow()
+        {
+            InitializeComponent();
+            firstShownTome.Text = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public new void Close()
+        {
+            _Close= true;
+            base.Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // If Close() was called, close the window (instead of hiding it)
+            if (_Close) return;
+
+            // Hide the window (instead of closing it)
+            e.Cancel = true;
+            Hide();
+        }
+
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            // Get date/time the window is shown now (ie when it becomes visible)
+            if ((bool) e.NewValue)
+            {
+                shownThisTime.Text = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
---
+++ WindowHiding/WindowHiding/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowHiding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ChildWindow _childWindow;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void showChildWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create the window if it's not already created
+            if (_childWindow == null)
+                _childWindow = new ChildWindow();
+            _childWindow.Show();
+        }
+
+        private void Window_Closing(object sender, RoutedEventArgs e)
+        {
+            // Close the child window only when this window closes
+            _childWindow.Close();
+        }
+    }
+}
