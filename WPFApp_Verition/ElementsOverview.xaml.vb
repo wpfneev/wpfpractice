53ce0b2cb92f8c158fce89f3781f489517bed164
Srinivas Kamineni <srinivas.k@neevsystems.com>
2023-02-13 20:43:58 +0530

    Elements Practicing

---
+++ WPFApp_Verition/ElementsOverview.xaml.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Net.Http
+
+Public Class ElementsOverview
+
+    Private Sub Hyperlink_RequestNavigate(sender As Object, e As RequestNavigateEventArgs)
+        Dim client As New HttpClient
+        Dim taskResult = client.GetAsync(e.Uri.AbsoluteUri)
+        Dim httpResponse = taskResult.GetAwaiter().GetResult()
+        Dim stringResult = httpResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult()
+        Console.WriteLine(stringResult)
+        ''System.Diagnostics.Process.Start(e.Uri.AbsoluteUri)
+    End Sub
+End Class
