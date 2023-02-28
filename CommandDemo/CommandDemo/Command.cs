44461de20194f252c6835bfaea2f9b187e1f01e1
Abhay Kumar Sharma <abhay.s@neevsystems.com>
2023-02-28 19:21:54 +0530

    Added by Abhay

---
+++ CommandDemo/CommandDemo/Command.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CommandDemo
+{
+    class Command : ICommand
+    {
+        Action<object> executeMethod;
+        Func<object, bool> canexecuteMethod;
+
+        public Command(Action<object> executeMethod, Func<object, bool> canexecuteMethod)
+        {
+            this.executeMethod = executeMethod;
+            this.canexecuteMethod = canexecuteMethod;
+        }
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return false;
+        }
+
+        public void Execute(object? parameter)
+        {
+            executeMethod(parameter);   
+        }
+    }
+}
---
+++ CommandDemo/CommandDemo/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace CommandDemo
+{
+    
+    class ViewModel
+    {
+        public ICommand myProperty { get; set; }
+        public ViewModel() {
+            myProperty = new Command(ExecuteMethod, canExecuteMethod);
+        }
+        private bool canExecuteMethod(object parameter)
+        {
+            return true;
+        }
+
+        private void ExecuteMethod(object parameter)
+        {
+            MessageBox.Show("No Code Behind!");
+        }
+    }
+}
