2edf227bf4840bb9fc3cbeb6fc6b5bad6068c5e7
Srinivas Kamineni <srinivas.k@neevsystems.com>
2023-03-08 14:57:28 +0530

    Windows APP Store clone

---
+++ WindowsStoreClone/UserControls/AnApp.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace WindowsStoreClone.UserControls
+{
+    /// <summary>
+    /// Interaction logic for AnApp.xaml
+    /// </summary>
+    public partial class AnApp : UserControl
+    {
+        public AnApp()
+        {
+            InitializeComponent();
+
+            //Need to pick the image randomly instead of static image source
+            string imagesFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images");
+            List<string> availImages = Directory.GetFiles(imagesFolder, "*.png").ToList();
+            Random rnd = new Random();
+            int imageId = rnd.Next(0,availImages.Count);
+            ProductImage.Source = new BitmapImage(new Uri(availImages[imageId], UriKind.RelativeOrAbsolute));
+            AppNameText.Text = Path.GetFileName(availImages[imageId]).Split("-").Last().Replace(".png", "");
+        }
+
+        private void ProductImage_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+    }
+}
