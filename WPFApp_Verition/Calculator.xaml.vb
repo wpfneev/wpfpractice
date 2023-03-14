28fe3d61aa50a50e72a0d7dd996e2d25564a9af6
Srinivas Kamineni <srinivas.k@neevsystems.com>
2023-03-14 21:08:53 +0530

    Calculator related window

---
+++ WPFApp_Verition/Calculator.xaml.vb
@@ -0,0 +1,46 @@
+﻿Public Class Calculator
+    Property Result As String
+    Property FinalResult As Decimal = 0D
+    Property OperatorND As String
+    Private Sub btn_Click(sender As Object, e As RoutedEventArgs) Handles btn0.Click, btn1.Click, btn2.Click, btn3.Click, btn4.Click,
+                                                                          btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click,
+                                                                          btneq.Click, btnp.Click, btnm.Click, btnx.Click, btnd.Click, btndt.Click
+
+        ''Get the button value
+        Result = DirectCast(sender, Button).Content
+
+        ''If the value is of operator then do the below activities
+        Select Case Result
+            Case "="
+                Select Case OperatorND
+                    Case "+"
+                        FinalResult = CInt(txtResult.Text) + FinalResult
+                    Case "-"
+                        FinalResult = FinalResult - CInt(txtResult.Text)
+                    Case "/"
+                        FinalResult = FinalResult / CInt(txtResult.Text)
+                    Case "*"
+                        FinalResult = CInt(txtResult.Text) * FinalResult
+                End Select
+                OperatorND = ""
+                txtResult.Text = FinalResult
+            Case "+"
+                OperatorND = "+"
+                FinalResult = txtResult.Text
+                txtResult.Text = ""
+            Case "-"
+                OperatorND = "-"
+                FinalResult = txtResult.Text
+                txtResult.Text = ""
+            Case "/"
+                OperatorND = "/"
+                FinalResult = txtResult.Text
+                txtResult.Text = ""
+            Case "x"
+                OperatorND = "*"
+                FinalResult = txtResult.Text
+                txtResult.Text = ""
+        End Select
+        If IsNumeric(Result) Then txtResult.Text &= Result
+    End Sub
+End Class
---
+++ WPFApp_Verition/WindowPropChanger.xaml.vb
@@ -0,0 +1,47 @@
+﻿Imports Microsoft.EntityFrameworkCore.Metadata.Internal
+
+Public Class WindowPropChanger
+    ''Changing the parent control property with the help of binding.. https://stackoverflow.com/questions/42659813/binding-wpf-user-control-to-parent-property
+    ''Placing controls next to another https://stackoverflow.com/questions/18235010/how-can-i-add-a-clear-button-to-a-textbox-in-wpf
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+        ' Add any initialization after the InitializeComponent() call.
+        Dim itList As New List(Of ItControlList)
+
+        Dim item As New ItControlList()
+        item.txtBox = "hello"
+        item.btnSearch = "one"
+
+        itList.Add(item)
+
+        icList.ItemsSource = itList
+    End Sub
+    Private Sub btn_Apply_Click(sender As Object, e As RoutedEventArgs)
+        ''Dim bindingExp As BindingExpression = txt_WindowTitle.GetBindingExpression(TextBox.TextProperty)
+        ''Window.GetWindow(Me).Title = txt_WindowTitle.Text
+        ''bindingExp.UpdateSource()
+    End Sub
+End Class
+
+Class ItControlList
+    Private _txtBox As String
+    Public Property txtBox() As String
+        Get
+            Return _txtBox
+        End Get
+        Set(ByVal value As String)
+            _txtBox = value
+        End Set
+    End Property
+    Private _btnSearch As String
+    Public Property btnSearch() As String
+        Get
+            Return _btnSearch
+        End Get
+        Set(ByVal value As String)
+            _btnSearch = value
+        End Set
+    End Property
+End Class
